a1efe9f7d785ed9596c6d216d1156a95c16cb204
shonosuke1 <a5825095@aoyama.ac.jp>
2026-06-19 17:38:07 +0900

    こみっと３

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ function addTask(title) {
  */
 function onUpdateIsDone(task) {
     console.log("チェックボックスがクリックされました。", task);
+    
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
     const currentValue = task.isDone;
